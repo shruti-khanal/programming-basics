cb8f0322337fad71b8899b6dc6eedac89f00aff0
shruti-khanal <shruti100725@gmail.com>
2026-01-19 17:56:27 +0530

    add sum program : sum.c

---
+++ C/4_sum.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main()
+{
+    
+    int a, b, c,d,sum;
+   printf("Enter four numbers: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    sum = a + b + c + d;
+    printf("Sum = %d", sum);
+    return 0;
+}
+
