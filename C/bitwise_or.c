4165d1ee550ef2e408ec78e09648f6df5c11914e
shruti-khanal <shruti100725@gmail.com>
2026-01-25 22:00:56 +0530

    add program to demonstrate bitwise operations using conditional statements

---
+++ C/bitwise_or.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main()
+{
+    if(-100&&100||20&&-20)
+    {
+        printf("%s", "condition is true .");
+    }
+    else
+    printf("%s", "condition is false.");
+ return 0;
+}
