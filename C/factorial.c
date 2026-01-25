fbd99e233c203c574a2dbcade422ae4ae81b3061
shruti-khanal <shruti100725@gmail.com>
2026-01-25 22:03:18 +0530

    add program to calculate factorial of a number

---
+++ C/factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main()
+{
+    
+
+    int n, i;
+     long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+
+    printf("Factorial of %d =%ld", n, fact);
+    return 0;
+}
