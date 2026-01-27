d0e21ac097be0e02989bd72fe94a5809a95f9c8b
shruti-khanal <shruti100725@gmail.com>
2026-01-27 20:19:37 +0530

    add C++ program to display name, branch, and college

---
+++ CPP/name.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    string name,branch,college_name;
+    name="shruti\n";
+    branch = "bca\n";
+    college_name="geu\n";
+    cout<<name<<branch<<college_name;
+}
