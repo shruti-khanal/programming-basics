f1cfffbebc906180013f7cddf6e599b1fae8af6d
shruti-khanal <shruti100725@gmail.com>
2026-02-13 18:54:29 +0530

    add c++ program to perform basic arithmetic operations

---
+++ CPP/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Shruti" << endl;
+
+    int a = 10, b = 3;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Modulus: " << a % b << endl;
+
+    return 0;
+}
+
