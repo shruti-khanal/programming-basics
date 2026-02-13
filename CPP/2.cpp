de92ac6c7077b38dd049cf4951cce80d9ea76fc1
shruti-khanal <shruti100725@gmail.com>
2026-02-13 18:52:57 +0530

    add c++ program to dispaly values in different data types

---
+++ CPP/2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Shruti" << endl;
+
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    const double pi = 3.14;
+
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Character: " << c << endl;
+    cout << "Constant PI: " << pi << endl;
+
+    return 0;
+}
