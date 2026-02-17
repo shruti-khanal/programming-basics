2d4cdf682926c936f9266c9b269c801439af0eb9
shruti-khanal <shruti100725@gmail.com>
2026-02-17 21:47:10 +0530

    add c++ program to show use of function

---
+++ CPP/9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int add(int, int);
+
+int main() {
+    cout << "Shruti" << endl;
+
+    int result = add(5, 7);
+    cout << "Sum: " << result;
+
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+
